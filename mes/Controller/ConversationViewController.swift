0fda40746563d45040d6bb815aeb2618f6bc9081
brightonlaw <lawchunhei88@hotmail.com>
2021-07-20 00:48:46 +0800

    init

---
+++ mes/Controller/ConversationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mes
+//
+//  Created by Chun Hei Law on 7/19/21.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
